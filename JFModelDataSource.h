94f6d2a48585561b34fc95de0ee7c6f60573a7bd
Joe Fabisevich <Joe@fabisevi.ch>
2014-02-28 22:31:56 -0500

    Initial commit

---
+++ JFModelDataSource.h
@@ -0,0 +1,43 @@
+//
+//  JFModelDataSource.h
+//  Picks
+//
+//  Created by Joe on 10/2/13.
+//  Copyright (c) 2013 mergesort. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Interface
+
+@interface JFModelDataSource : NSObject
+
+@property (nonatomic) NSArray *items;
+
+/**
+ *  A method which populates a data source by providing JSON, which contains an array of objects
+ *  to parse, and a block which executes the parsing. Only call this directly in subclasess.
+ *
+ *  @param JSONToParse      The JSON containing an array of objects to parse
+ *  @param JSONPopulator    The mechanism for parsing all the objects
+ *
+ *  @return A JFModelDataSource object, containing objects which are parsed
+ */
++ (instancetype)populateDataSourceFromJSON:(id)JSONToParse withParserBlock:(void (^)(JFModelDataSource *dataSource, id JSON))JSONPopulator fromSender:(id)sender;
+
+/**
+ *  A method which populates a data source by providing JSON
+ *
+ *  @param JSON The JSON containing an array of objects to parse
+ *
+ *  @return A JFModelDataSource object, containing objects which are parsed
+ */
++ (instancetype)populateDataSourceFromJSON:(id)JSON;
+
+- (void)updateFromDataSource:(JFModelDataSource *)dataSource;
+
+- (id)objectAtIndexedSubscript:(NSUInteger)idx;
+
+@end
---
+++ JFModelDataSource.m
@@ -0,0 +1,107 @@
+//
+//  JFModelDataSource.m
+//  Picks
+//
+//  Created by Joe on 10/2/13.
+//  Copyright (c) 2013 mergesort. All rights reserved.
+//
+
+#import "JFModelDataSource.h"
+
+
+@implementation JFModelDataSource
+
+- (id)init
+{
+	if ([self class] == [JFModelDataSource class])
+	{
+		NSAssert(NO, @"You cannot init this class directly. Instead, use a subclass.");
+		return nil;
+	}
+	else
+    {
+        self = [super init];
+        
+        if (self)
+        {
+            [self setup];
+        }
+    }
+    
+    return self;
+}
+
+- (id)initInProtectedScope
+{
+    self = [super init];
+    
+    if (self)
+    {
+        [self setup];
+    }
+    
+    return self;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Setup
+
+- (void)setup
+{
+    self.items = [NSArray array];
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Populate DataSource
+
++ (instancetype)populateDataSourceFromJSON:(id)JSONToParse withParserBlock:(void (^)(JFModelDataSource *dataSource, id JSON))JSONPopulator fromSender:(id)sender
+{
+    if ([self class] == [JFModelDataSource class] || [[sender class] isSubclassOfClass:[JFModelDataSource class]])
+    {
+        id dataSource = [[JFModelDataSource alloc] initInProtectedScope];
+        
+        if (JSONPopulator)
+        {
+            JSONPopulator(dataSource, JSONToParse);
+        }
+        else
+        {
+            return nil;
+        }
+        
+        return dataSource;
+    }
+    else
+    {
+        NSAssert(NO, @"You cannot call this method directly outside of JFModelDataSource.");
+        return nil;
+    }
+}
+
++ (instancetype)populateDataSourceFromJSON:(id)JSON
+{
+    NSAssert(NO, @"This method must be implemented in a subclass.");
+    return nil;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Public methods
+
+- (void)updateFromDataSource:(JFModelDataSource *)dataSource
+{
+    self.items = [dataSource.items copy];
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Object At Index Subscript
+
+- (id)objectAtIndexedSubscript:(NSUInteger)idx
+{
+    return (self.items.count > idx) ? self.items[idx] : nil;
+}
+
+@end
